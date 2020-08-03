9be7274e623653d0b0f0032cc2eb24776665012d
Austen Cameron <austen.cameron@gmail.com>
2020-08-03 14:27:13 -0600

    use exists

--- resources/views/components/streak-badge.blade.php
+++ resources/views/components/streak-badge.blade.php
@@ -1,6 +1,6 @@
 @props(['habit'])
 
-@if ($habit->currentStreak->length ?? false)
+@if ($habit->currentStreak->exists)
     <div class="inline-flex text-sm items-center py-px px-3 bg-teal-200 text-teal-700 rounded-full">
         {{ $habit->currentStreak->length }} day streak!
     </div>
