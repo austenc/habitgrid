b2c40c8de792154ba691ed4753c852ffa0af4389
Austen Cameron <austen.cameron@gmail.com>
2020-09-24 13:44:17 -0600

    initial landing page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <div class="py-6 flex items-center justify-between">
             {{-- Logo --}}
             <div>
-                <svg class="h-12" viewBox="0 0 149 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg class="h-8 md:h-12" viewBox="0 0 149 38" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M53.8966 9.48191V28.1211H50.9144V20.069H43.1605V28.1211H40.1782V9.48191H43.1605V17.713H50.9144V9.48191H53.8966ZM62.1172 12.9115C63.489 12.9115 64.5726 13.0507 65.3679 13.329C66.1632 13.5875 66.7298 14.0249 67.0678 14.6412C67.4256 15.2377 67.6046 16.0528 67.6046 17.0867V28.1211H66.2327L65.4275 26.63C64.7118 27.2463 63.9165 27.7036 63.0417 28.0018C62.1868 28.2801 61.2026 28.4193 60.0893 28.4193C58.4391 28.4193 57.266 28.0813 56.5702 27.4053C55.8743 26.7095 55.5264 25.5563 55.5264 23.9459C55.5264 22.4945 55.7252 21.4507 56.1228 20.8145C56.5205 20.1783 57.2959 19.7509 58.449 19.5322C59.622 19.3135 61.5307 19.2041 64.175 19.2041H64.6223V15.5956C63.8867 15.3769 62.8827 15.2675 61.6102 15.2675C60.0197 15.2675 58.3397 15.5061 56.5702 15.9833L56.1825 13.8062C56.9181 13.5477 57.8327 13.339 58.9262 13.1799C60.0197 13.001 61.0834 12.9115 62.1172 12.9115ZM62.2365 26.123C63.1709 26.123 63.7972 26.0037 64.1153 25.7651C64.4533 25.5265 64.6223 25.0692 64.6223 24.3933V21.1426H60.6559C59.7214 21.1426 59.0852 21.2619 58.7472 21.5005C58.4092 21.739 58.2403 22.1963 58.2403 22.8723V26.123H62.2365ZM77.2648 12.7624C78.7361 12.7624 79.8494 12.9811 80.6049 13.4185C81.3605 13.8559 81.8873 14.6313 82.1856 15.7447C82.4838 16.8581 82.6329 18.4983 82.6329 20.6654C82.6329 22.8127 82.4639 24.433 82.1259 25.5265C81.8078 26.6001 81.2312 27.3556 80.3962 27.793C79.5612 28.2106 78.3185 28.4193 76.6684 28.4193H76.6385C75.4456 28.4193 74.3521 28.3497 73.358 28.2106C72.3639 28.0714 71.2903 27.8328 70.1372 27.4948V8.289L73.1195 7.93113V13.4781C73.5767 13.2197 74.1334 13.0407 74.7895 12.9413C75.4456 12.822 76.2707 12.7624 77.2648 12.7624ZM79.5015 15.2079H74.4913C73.9545 15.2079 73.5867 15.2576 73.3879 15.357C73.2089 15.4564 73.1195 15.6552 73.1195 15.9534V25.9739H78.1297C78.6466 25.9739 79.0045 25.9341 79.2033 25.8546C79.4021 25.7552 79.5015 25.5961 79.5015 25.3774V15.2079ZM87.4145 12.9712V28.1211H84.4323V13.4185L87.4145 12.9712ZM85.9532 10.6152C85.2176 10.6152 84.7206 10.4959 84.4621 10.2573C84.2235 9.99883 84.1042 9.52167 84.1042 8.82581C84.1042 8.11006 84.2335 7.62296 84.4919 7.36449C84.7504 7.10603 85.2375 6.9768 85.9532 6.9768C86.6491 6.9768 87.1163 7.10603 87.3549 7.36449C87.6134 7.62296 87.7426 8.11006 87.7426 8.82581C87.7426 9.52167 87.6233 9.99883 87.3847 10.2573C87.1461 10.4959 86.669 10.6152 85.9532 10.6152ZM93.9837 15.2675V25.9142C94.6994 25.9937 95.4052 26.0434 96.1011 26.0633L95.9222 28.0913C94.8088 28.31 93.9042 28.4193 93.2083 28.4193C92.4528 28.4193 91.8961 28.2404 91.5382 27.8825C91.1804 27.5048 91.0014 26.9381 91.0014 26.1826V15.2675H88.9138V13.2097H91.0014V10.0187L93.9837 9.27315V13.2097H96.6379V15.2675H93.9837ZM115.114 17.8621V27.5246C114.139 27.9024 113.125 28.1708 112.072 28.3299C111.038 28.4889 109.795 28.5684 108.344 28.5684C106.535 28.5684 105.193 28.3199 104.318 27.8229C103.443 27.3258 102.837 26.4013 102.499 25.0494C102.161 23.6974 101.992 21.6197 101.992 18.8164C101.992 16.1523 102.19 14.1342 102.588 12.7624C102.986 11.3906 103.691 10.4263 104.705 9.8696C105.739 9.31291 107.27 9.03457 109.298 9.03457C110.074 9.03457 110.958 9.09421 111.952 9.2135C112.966 9.33279 113.781 9.48191 114.398 9.66084L114.01 12.1063C113.215 11.9472 112.27 11.818 111.177 11.7186C110.103 11.6192 109.229 11.5695 108.553 11.5695C107.28 11.5695 106.395 11.7186 105.898 12.0168C105.401 12.2952 105.153 12.9016 105.153 13.836V26.1826H112.459V20.2181H109.626V17.8621H115.114ZM119.507 15.5657C120.401 14.7705 121.167 14.1541 121.803 13.7167C122.439 13.2793 123.036 12.9612 123.593 12.7624L123.891 15.685C123.235 15.8838 122.549 16.1622 121.833 16.5201C121.137 16.8581 120.541 17.2159 120.044 17.5937V28.1211H117.061V13.2097H118.463L119.507 15.5657ZM127.32 12.9712V28.1211H124.338V13.4185L127.32 12.9712ZM125.859 10.6152C125.123 10.6152 124.626 10.4959 124.367 10.2573C124.129 9.99883 124.01 9.52167 124.01 8.82581C124.01 8.11006 124.139 7.62296 124.397 7.36449C124.656 7.10603 125.143 6.9768 125.859 6.9768C126.554 6.9768 127.022 7.10603 127.26 7.36449C127.519 7.62296 127.648 8.11006 127.648 8.82581C127.648 9.52167 127.529 9.99883 127.29 10.2573C127.051 10.4959 126.574 10.6152 125.859 10.6152ZM141.613 7.93113V28.1211H140.241L139.585 26.9282C139.188 27.5445 138.611 27.972 137.855 28.2106C137.12 28.4491 136.096 28.5684 134.784 28.5684C133.193 28.5684 132 28.3597 131.205 27.9422C130.43 27.5246 129.883 26.7691 129.565 25.6756C129.267 24.5821 129.117 22.9121 129.117 20.6654C129.117 18.5579 129.267 16.9575 129.565 15.864C129.863 14.7705 130.38 14.005 131.116 13.5676C131.851 13.1302 132.935 12.9115 134.366 12.9115C135.38 12.9115 136.225 12.9712 136.901 13.0905C137.597 13.1899 138.174 13.3688 138.631 13.6273V8.289L141.613 7.93113ZM138.631 15.357H133.621C133.084 15.357 132.716 15.4067 132.517 15.5061C132.338 15.6055 132.249 15.8043 132.249 16.1025V26.123H137.259C137.796 26.123 138.154 26.0733 138.333 25.9739C138.531 25.8745 138.631 25.6756 138.631 25.3774V15.357Z" fill="#475569"/>
                 <rect x="123.09" y="4.71777" width="5.35343" height="5.97577" rx="0.608935" fill="#3F83F8"/>
                 <rect x="123.09" y="4.71777" width="5.35343" height="5.97577" rx="0.608935" fill="url(#paint0_linear)" style="mix-blend-mode:overlay"/>
@@ -109,32 +109,32 @@
                 </defs>
                 </svg>
             </div>
-            <div class="space-x-8 text-lg font-semibold text-gray-500">
-                <a href="{{ route('register') }}" class="hover:text-primary-500">Sign Up</a>
-                <a href="{{ route('login') }}" class="transition duration-700 bg-primary-500 hover:bg-primary-700 text-primary-50 rounded px-4 py-3">Log In</a>
+            <div class="space-x-8 md:text-lg font-semibold text-gray-500">
+                <a href="{{ route('register') }}" class="hidden md:inline-block hover:text-primary-500">Sign Up</a>
+                <a href="{{ route('login') }}" class="inline-block transition duration-700 bg-primary-500 hover:bg-primary-700 text-primary-50 rounded px-4 py-3">Log In</a>
             </div>
         </div>
     </div>
 
     {{-- Hero --}}
-    <div class="relative pb-32 pt-48">
+    <div class="relative pb-32 pt-20 md:pt-48">
         <div class="absolute inset-y-0 w-full z-10">
             <img class="object-cover object-center top-0 w-full h-full" src="/img/hero-bg.jpg" alt="Habit Grid">
         </div>
         <div class="py-10 bg-gradient-to-r from-primary-50 to-transparent relative z-20">
             <div class="container">
-                <h1 class="text-5xl font-extrabold tracking-tighter text-gray-700">Improve your Habits, Get More Done</h1>
-                <h2 class="mt-5 text-gray-500 text-3xl tracking-tight">Focus on little changes, see big improvements</h2>
-                <a href="{{ route('register') }}" class="inline-block px-5 py-4 mt-8 font-semibold text-xl rounded text-primary-50 bg-primary-500 hover:bg-primary-700 transition duration-700">Get Started</a>
+                <h1 class="text-2xl md:text-5xl font-extrabold tracking-tighter text-gray-700">Improve your Habits,<br class="md:hidden"> Get More Done</h1>
+                <h2 class="mt-5 text-gray-500 text-xl md:text-3xl tracking-tight">Focus on little changes, see big improvements</h2>
+                <a href="{{ route('register') }}" class="inline-block px-5 py-4 mt-8 font-semibold text-base md:text-xl rounded text-primary-50 bg-primary-500 hover:bg-primary-700 transition duration-700">Get Started</a>
             </div>
         </div>
     </div>
 
     {{-- Main Content --}}
-    <div class="bg-cool-gray-100 shadow-inner pt-16">
+    <div class="bg-cool-gray-100 shadow-inner pt-16 pb-4">
         <div class="container">
-            <div class="flex space-x-8">
-                <div class="w-2/5">
+            <div class="space-y-5 md:space-y-0 md:flex md:space-x-8">
+                <div class="w-full md:w-2/5">
                     <h3 class="font-semibold text-xl tracking-tight text-gray-700">Track one habit, or dozens</h3>
                     <p class="mt-4 text-gray-500 text-lg">
                         You can track as many habits as you want. Whether you’re trying to drink more water, work out more consistently, or simply call mom more often. 
@@ -143,19 +143,19 @@
                         This simple tool makes it easy to see how you’re doing and track your progress. 
                     </p>
                 </div>
-                <div class="w-3/5 -mb-12">
-                    <img src="/img/habit-list.jpg" class="block w-full rounded shadow-lg" alt="Habit List View">
+                <div class="w-full md:w-3/5">
+                    <img src="/img/habit-list.jpg" class="block md:-mb-16 w-full rounded shadow-lg" alt="Habit List View">
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="container pt-32">
-        <div class="flex space-x-8">
-            <div class="w-3/5">
+    <div class="container pt-8 md:pt-32">
+        <div class="flex flex-col-reverse space-y-reverse md:flex-row space-y-6 md:space-y-0 md:flex md:space-x-8">
+            <div class="md:w-3/5">
                 <img src="/img/habit-list.jpg" class="w-full rounded shadow-lg" alt="Habit List View">
             </div>
-            <div class="w-2/5">
+            <div class="md:w-2/5">
                 <h3 class="font-semibold text-xl tracking-tight text-gray-700">See your year at a glance</h3>
                 <p class="mt-4 text-gray-500 text-lg">
                     Trying to improve your habits? With this habit tracker, you’ll be able to easily see and manage individual habits at a glance. With the github-inspired grid view, you can stay in the loop with your improvements across the entire year.
@@ -169,18 +169,18 @@
 
     {{-- Footer --}}
     <div class="container py-10 mt-20">
-        <div class="flex justify-between">
-            <div class="w-1/3 font-marker text-gray-400">
+        <div class="flex flex-col-reverse space-y-5 space-y-reverse md:space-y-0 md:flex-row justify-between">
+            <div class="w-full text-center md:text-left md:w-1/3 font-marker text-gray-400">
                 Built by <a href="https://twitter.com/austencam" class="transition duration-700 text-primary-400 hover:text-primary-700">@AUSTENCAM</a> with 
                 <svg class="inline-block" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M5.59371 3.73923C5.85669 -3.25892 13.8707 1.92654 10.4338 5.41435C10.421 5.42736 10.4081 5.44224 10.3972 5.4569C8.69147 7.75363 7.61845 8.98756 5.59377 11.1243C5.59375 11.1244 5.59371 11.1243 5.59371 11.1243V11.1243C5.59371 11.1243 5.59371 11.1243 5.5937 11.1243C3.49587 9.0511 2.43521 7.8317 0.991162 5.43452C-1.10386 2.61263 5.59371 -1.47908 5.59371 3.73914V3.73923Z" fill="#C72121"/>
                 </svg>
             </div>
-            <div class="w-1/3 text-center divide-x space-x-3 divide-gray-400 font-semibold">
+            <div class="w-full md:w-1/3 text-center divide-x space-x-3 divide-gray-400 font-semibold">
                 <a class="inline-block text-link" href="{{ route('register') }}">Sign Up</a>
                 <a class="pl-3 inline-block text-link" href="{{ route('login') }}">Log In</a>
             </div>
-            <div class="w-1/3">
+            <div class="w-full md:w-1/3">
                 <div class="relative text-right w-full">
                     <a href="https://github.com/austenc/habitgrid/fork" class="absolute top-0 right-0 -mt-16 mr-4 text-primary-400 hover:text-primary-700">
                         <svg height="60" viewBox="0 0 155 46" fill="none" xmlns="http://www.w3.org/2000/svg">
