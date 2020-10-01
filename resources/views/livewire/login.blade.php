86b4493b3b9a0a2de157deb7b0bc4890d3343482
Austen Cameron <austen.cameron@gmail.com>
2020-10-01 11:24:56 -0600

    accidentally a word

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -15,7 +15,7 @@
             </div>
         </x-card>
         <div class="text-center mt-5">
-            Need account? <a href="{{ route('register') }}" class="text-link font-medium">Sign up here</a>
+            Need an account? <a href="{{ route('register') }}" class="text-link font-medium">Sign up here</a>
         </div>
     </x-form>
 </div>
